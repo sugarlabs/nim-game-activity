971ba33047a6afd46b2196066f38024c26f2ecae
Spandan Barve <spandan567@gmail.com>
2024-06-09 11:58:27 +0000

    first activity launch

---
+++ config.py
@@ -0,0 +1,40 @@
+# Copyright (C) 2024 Spandan Barve
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
+import os
+import pygame
+from font import Font
+
+
+font = Font()
+background_color = (238, 227, 214)
+front_color = (0, 0, 0)                                                    
+images = {}
+
+def load_images():
+    for dir in ["object", "robot"]:
+        dir_images = {}
+        directory = f"assets/images/{dir}"
+        for filename in os.listdir(directory):
+            if filename.endswith(".png") or filename.endswith(".jpg") or filename.endswith(".jpeg"):
+                file_path = os.path.join(directory, filename)
+                try:
+                    image = pygame.image.load(file_path)
+                    base_filename = os.path.splitext(filename)[0]
+                    dir_images[base_filename] = image
+                except pygame.error as err:
+                    print(f"Error loading image '{file_path}': {err}")
+        images[dir] = dir_images
---
+++ main.py
@@ -0,0 +1,102 @@
+# Copyright (C) 2024 Spandan Barve
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
+import pygame
+import gi
+gi.require_version('Gtk', '3.0')
+from gi.repository import Gtk
+import config
+from views import menu
+from components.help import Help
+
+
+class RiverCrossing:
+    def __init__(self):
+        self.running = True
+        self.clock = pygame.time.Clock()
+
+        self.gameDisplay = None
+        self.info = None
+        self.update_function = None
+        self.bg = (0, 0, 0)
+
+        self.help_popup = None
+
+    def vw(self, x):
+        return (x / 100) * self.display_rect.width
+
+    def vh(self, y):
+        return (y / 100) * self.display_rect.height
+
+    def blit_centred(self, surf, x, y):
+        rect = surf.get_rect()
+        centered_coords = (x - rect.width // 2, y - rect.height // 2)
+        self.gameDisplay.blit(surf, centered_coords)
+
+    def stop(self):
+        self.running = False
+
+    def set_screen(self, view):
+        view(self)
+
+    def set_background(self, color):
+        self.bg = color
+
+    def show_help(self):
+        self.help_popup.show()
+
+    def hide_help(self):
+        self.help_popup.hide()
+
+    def run(self):
+        self.gameDisplay = pygame.display.get_surface()
+        self.info = pygame.display.Info()
+        self.display_rect = self.gameDisplay.get_rect()
+
+        config.load_images()
+        self.bg = config.background_color
+        self.set_screen(menu.view)
+        # self.help_popup.initialize()
+
+        # if not (self.gameDisplay):
+        #     self.gameDisplay = pygame.display.set_mode(
+        #         (self.info.current_w, self.info.current_h))
+        #     pygame.display.set_caption("River Crossing Puzzle")
+
+        while self.running:
+            self.gameDisplay.fill(self.bg)
+
+            if self.update_function is not None:
+                self.update_function()
+
+            while Gtk.events_pending():
+                Gtk.main_iteration()
+
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    break
+
+            pygame.display.update()
+            self.clock.tick(60)
+
+        return
+
+
+if __name__ == "__main__":
+    pygame.init()
+    pygame.display.set_mode((0, 0), pygame.RESIZABLE)
+    game = RiverCrossing()
+    game.run()
