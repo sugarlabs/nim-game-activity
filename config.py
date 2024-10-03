2a95fa70ede3cacb359fe21f4f5ec6e06d207bd5
Spandan Barve <spandan567@gmail.com>
2024-10-03 06:52:48 +0000

    Update theory

--- config.py
+++ config.py
@@ -24,7 +24,7 @@ background_color = (238, 227, 214)
 front_color = (0, 0, 0)                                                    
 accent_color = (190, 100, 55)
 images = {}
-difficulty = 0 # 0->Easy 1->Medium 2->Difficult
+difficulty = 1 # 0->Easy 1->Medium 2->Difficult
 
 current_object = "coin"
 current_robot = "tin"
